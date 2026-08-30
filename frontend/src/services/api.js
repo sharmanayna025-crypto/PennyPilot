eefc92c5b0db3e5734c58d096c561f73423fde91
Nayna Sharma <sharmanayna025@gmail.com>
2026-08-30 20:42:10 +0530

    Configure frontend for production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8080",
 });
 
 API.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
@@ -13,7 +12,7 @@ API.interceptors.request.use((config) => {
   }
 
   return config;
-
 });
 
 export default API;
+
